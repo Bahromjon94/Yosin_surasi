ac3eeedc9f0bfb2667ee301beebade37e0e9b5c1
Bahromjon94 <bahromjon.ergashboyev@gmail.com>
2021-01-31 21:56:39 +0500

    Ba'zi narsalar qo'shildi

--- app/src/main/java/uz/getitpro/yosinsurasi/MainActivity.java
+++ app/src/main/java/uz/getitpro/yosinsurasi/MainActivity.java
@@ -1,16 +1,35 @@
 package uz.getitpro.yosinsurasi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIME = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        
-        Toast.makeText(this, "Bismillahir Rohmanir Rohim", Toast.LENGTH.SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(MainActivity.this, ReadableActivity.class));
+                    finish();
+                }
+            }, SPLASH_TIME);
+
     }
+
 }
